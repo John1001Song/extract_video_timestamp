3e2db803cc05a335d1a099538d8b855d46025e5a
Jinyue Song <jysong@ucdavis.edu>
2024-08-23 16:56:18 -0700

    update code to extract frames from multiple videos

--- code/extract_frames.py
+++ code/extract_frames.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import glob
 
 def extract_frames(video_path, output_folder, frame_interval=30):
     """
@@ -21,12 +22,13 @@ def extract_frames(video_path, output_folder, frame_interval=30):
     cap = cv2.VideoCapture(video_path)
     
     if not cap.isOpened():
-        print("Error: Could not open video.")
+        print(f"Error: Could not open video {video_path}.")
         return
     
     frame_count = 0
     extracted_count = 0
-    
+    video_basename = os.path.basename(video_path).split('.')[0]  # Get the base name without extension
+
     while cap.isOpened():
         ret, frame = cap.read()
         
@@ -35,7 +37,7 @@ def extract_frames(video_path, output_folder, frame_interval=30):
         
         # Save the frame if it is at the specified interval
         if frame_count % frame_interval == 0:
-            frame_filename = os.path.join(output_folder, f"frame_{extracted_count:04d}.jpg")
+            frame_filename = os.path.join(output_folder, f"{video_basename}_frame_{extracted_count:04d}.jpg")
             cv2.imwrite(frame_filename, frame)
             print(f"Saved frame {extracted_count} to {frame_filename}")
             extracted_count += 1
@@ -43,10 +45,16 @@ def extract_frames(video_path, output_folder, frame_interval=30):
         frame_count += 1
     
     cap.release()
-    print(f"Finished extracting {extracted_count} frames from the video.")
+    print(f"Finished extracting {extracted_count} frames from the video {video_path}.")
 
 # Usage example
-video_path = '../data/video_for_frames_only/TimeVideo_20240822_114913.mp4'
-output_folder = '../data/output/frames_only/'
-frame_interval = 30  # Extract one frame every 30 frames
-extract_frames(video_path, output_folder, frame_interval)
+video_folder = '../data/video_for_calibration/'
+output_folder = '../data/output/calibration_frames/'
+frame_interval = 10  # Extract one frame every 10 frames
+
+# Find all MP4 files in the directory
+video_files = glob.glob(os.path.join(video_folder, '*.mp4'))
+
+# Extract frames from each video file
+for video_file in video_files:
+    extract_frames(video_file, output_folder, frame_interval)
